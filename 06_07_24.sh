c43fd73d3aa56b7adf05a8dc6806dcdb22ddf8a5
etaylor9 <etaylor9@stanford.edu>
2024-06-07 11:25:59 -0700

    Square chip with hexagonal lattice points. f = 1/3

---
+++ 06_07_24.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name= hexagonalLattice
+#SBATCH --output=slurm-%A.out
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8GB
+#SBATCH --time=48:00:00
+
+set -x
+
+module load media ffmpeg
+
+# GROUP_HOME="."
+
+# Activate conda env
+source $GROUP_HOME/miniconda3/bin/activate
+conda activate TDGL
+
+export LD_LIBRARY_PATH=${CONDA_PREFIX}/lib:${LD_LIBRARY_PATH}
+
+# GROUP_SCRATCH="."
+outdir=$GROUP_SCRATCH/simulations/results/06_07_24/$SLURM_JOB_ID
+mkdir -p $outdir
+
+pyscript=$HOME/scripts/06_07_24.py #HAVE TO ACTUALLY PUT THE SCRIPT NAME HERE
+
+# Copy the python script and this shell script to the results directory
+cp -u $pyscript $outdir/
+cp -u $0 $outdir/
+
+python $pyscript \
+    --directory=$outdir
+
+# Move the stdout log to the results directory
+mv "slurm-${SLURM_JOB_ID}.out" $outdir
