18ccf50b05ff06371a1d6c1835cefcc6d050c612
etaylor9 <etaylor9@stanford.edu>
2024-08-12 16:55:13 -0700

    kagome lattice

--- 08_12_24/08_12_24_kagome.sh
+++ 08_12_24/08_12_24_kagome.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=NbN
+#SBATCH --job-name=Kagome
 #SBATCH --output=slurm-%A.out
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=16GB
@@ -16,29 +16,30 @@ conda activate TDGL
 export LD_LIBRARY_PATH=${CONDA_PREFIX}/lib:${LD_LIBRARY_PATH}
 
 # Create output directory in scratch space
-outdir=$GROUP_SCRATCH/simulations/results/07_31_24/NbN/$SLURM_JOB_ID
+outdir=$GROUP_SCRATCH/simulations/results/08_12_24/kagome/$SLURM_JOB_ID
 mkdir -p $outdir
 
 # Specify the Python script path
-pyscript=$HOME/scripts/07_31_24/07_31_24_NbN.py
+pyscript=$HOME/scripts/08_12_24/08_12_24_kagome.py
 
 # Copy the Python script and this shell script to the results directory
 cp -u $pyscript $outdir/ || { echo "Failed to copy Python script"; exit 1; }
 cp -u $0 $outdir/ || { echo "Failed to copy shell script"; exit 1; }
 
-# Define the value of f
-f="1/4"
-coherence_length="60"
-london_lambdas=("50" "100" "200" "300")
+# Define the values of f as fractions
+fs=("1/4" "1/2" "1/3") 
 
-for london_lambda in "${london_lambdas[@]}"; do
+for f in "${fs[@]}"; do
     # Evaluate the fraction using awk
     f_decimal=$(awk "BEGIN {print $f}")
 
     # Run the Python script with the --f and --outdir arguments
-    python $pyscript --f $f_decimal --coherence_length $coherence_length --london_lambda $london_lambda --outdir $outdir || { echo "Python script failed for coherence_length $coherence_length"; exit 1; }
+    python $pyscript --f $f_decimal --outdir $outdir
 done
 
+
+# Move the stdout log to the results directory
+mv "slurm-${SLURM_JOB_ID}.out" $outdir
 
 # Move the stdout log to the results directory
 mv "slurm-${SLURM_JOB_ID}.out" $outdir || { echo "Failed to move slurm output log"; exit 1; }
